ed4fa178865bade96c061bf33142d556ac2a2ec1
Valentin Andreev <onuswarm@gmail.com>
2018-07-17 15:42:03 +0300

    corrected

--- src/test/java/andreev/test/FirstTest.java
+++ src/test/java/andreev/test/FirstTest.java
@@ -13,27 +13,30 @@ import java.util.concurrent.TimeUnit;
 
 public class FirstTest {
     public static WebDriver driver;
+
     @BeforeClass
-    public static void setup(){
-        System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver.exe");
-        driver = new ChromeDriver() ;
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\loki\\IdeaProjects\\andreev.test\\chromedriver.exe");
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://yandex.ru");
-        }
-    @Test
-     public void firstTest(){
+    }
 
-          WebElement searchfield = driver.findElement(By.id("text"));
-          searchfield.sendKeys("Погода Пенза");
-          WebElement searchbutton = driver.findElement(By.cssSelector(".search2__button button"));
-          searchbutton.click();
-          WebElement firststring = driver.findElement(By.cssSelector("a[accesskey='1']"));
-          String pogoda = firststring.getText();
-          Assert.assertEquals("Погода в Пензе", pogoda);
-          }
-          @AfterClass
-    public static void end(){
-              driver.quit();
-          }
+    @Test
+    public void firstTest() {
+
+        WebElement searchField = driver.findElement(By.id("text"));
+        searchField.sendKeys("Погода Пенза");
+        WebElement searchButton = driver.findElement(By.cssSelector(".search2__button button"));
+        searchButton.click();
+        WebElement firstString = driver.findElement(By.cssSelector("a[accesskey='1']"));
+        String weather = firstString.getText();
+        Assert.assertEquals("Погода в Пензе", weather);
+    }
+
+    @AfterClass
+    public static void end() {
+        driver.quit();
+    }
 }
